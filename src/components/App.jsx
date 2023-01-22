8d29fab4aa0bdcada5296f98549937211975c145
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-22 16:55:22 +0530

    use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,8 @@ import Emojjis from './EmojJis';
 import notes from './notes';
 import Login from './Login';
 import Register from './Register';
+import cars from './practice';
+import animals from './data';
 
 
 // function createCard(contact){
@@ -45,24 +47,98 @@ import Register from './Register';
 //     );
 // }
 
+// let [honda ,tesla]=cars
+// var {speedStats : {topSpeed:teslaTopSpeed},coloursByPopularity: [teslaTopColour]}=tesla;
+
+// var {speedStats : {topSpeed:hondaTopSpeed},coloursByPopularity: [hondaTopColour]}=honda;
+
+
+
+// const [headingText,setHeadingText]=useState("Hello")
+// const [bgColor,setBgColor]=useState("white");
+// function handleClick(){
+//     setHeadingText("submitted");
+
+// }
+// function over(){
+//     setBgColor("black");
+// }
+// function out(){
+//     setBgColor("white");
+// }
+
 function App(){
-    const [count,setCount]=useState(0);
-    function increase(){
-        setCount(count+1);
+    const [name,setName]=useState("");
+    const [headingText,setHeading]=useState("");
+    function handleChange(event){
+        setName(event.target.value);
     }
-    function decrease(){
-        setCount(count-1);
+    function handleClick(event){
+        setHeading(name);
+
+        event.preventDefault();
     }
     return (
-        <div className='container'>
-        <h1>{count}</h1>
-        <button onClick={increase}>+</button>
-        <button onClick={decrease}>-</button>
+        <div className="container">
+          <h1>Hello {headingText}</h1>
+          <form onSubmit={handleClick}>
+            <input onChange={handleChange} type="text" value={name} placeholder="What's your name?" />
+            <button type="submit" >Submit</button>
+          </form>
         </div>
-    );
-}
-
-export default App;
+      );
+    }
+    
+    export default App;
+    {/* <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button style={{backgroundColor:bgColor}} onMouseOver={over} onMouseOut={out} onClick={handleClick}>Submit</button>
+    </div> */}
+    //   <table>
+    //     <tr>
+    //       <th>Brand</th>
+    //       <th>Top Speed</th>
+    //     </tr>
+    //     <tr>
+    //       <td>{tesla.model}</td>
+    //       <td>{teslaTopSpeed}</td>
+    //       <td>{teslaTopColour}</td>
+    //     </tr>
+    //     <tr>
+    //       <td>{honda.model}</td>
+    //       <td>{hondaTopSpeed}</td>
+    //       <td>{hondaTopColour}</td>
+    //     </tr>
+    //   </table>
+// let now=new Date().toTimeString().slice(0,8)
+// const [time,setTime]=useState(now);
+// console.log(now);
+// const updateTime=()=>{
+//     const newTime=new Date().toTimeString().slice(0,8)
+//     setTime(newTime);
+// }
+// setInterval(updateTime, 1000);
+// return (
+//     <div className='container'>
+//         <h1>{time}</h1>
+//         <button onClick={updateTime}>Get Time</button>
+//     </div>
+// );
+// const [count,setCount]=useState(0);
+// function increase(){
+//     setCount(count+1);
+// }
+// function decrease(){
+//     setCount(count-1);
+// }
+// return (
+//     <div className='container'>
+//     <h1>{count}</h1>
+//     <button onClick={increase}>+</button>
+//     <button onClick={decrease}>-</button>
+//     </div>
+// );
 
 {/* <Header />
 {notes.map(data => <Note key={data.key} title={data.title} content={data.content}/>)}
